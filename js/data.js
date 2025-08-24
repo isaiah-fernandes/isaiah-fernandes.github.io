761eefb40d67e733c331b87ec33844b0b7790851
Isaiah Fernandes <isaiahfernandes799@gmail.com>
2025-08-24 15:16:39 -0500

    update:resume

--- js/data.js
+++ js/data.js
@@ -5,7 +5,6 @@ const siteData = {
     name: "Isaiah Fernandes",
     jobTitle: "Mechanical Engineer",
     exp: "",
-    sp: "Specializing in Oil and Gas",
     taglines: [
         "I build...",
         "I design...",
@@ -21,17 +20,17 @@ const siteData = {
         "I engineer..."
     ],
     about: [
-        "                       HOWDY                      ",
+        "HOWDY!",
         
         "I'm Isaiah and I'm your guy if you want to get stuff done.",
 
-        "Coming from a background of Mechanical Engineer. My experience spans oil and gas processing, multiphase boosting, field operations, and pipeline engineering backed by research that will contribute to improving oil production rates by up to 20%. As published, my work—centered around my specialization in process optimization and efficiency improvement—focuses on enhancing oil and gas production.",
+        "I’m someone who’s curious, creative, and always learning or exploring something new. With a passion for optimizing things, I’m always down to learn and try new things. This space is where I share a bit of what I do, what I have done, what I'm working on, and things that inspire me. Feel free to poke around!",
 
-        "I have worked across project design, simulation, execution (Pre-FEED & FEED) particularly in upstream and midstream environments. I bring hands-on knowledge of engineering software (CFD, FEA and AutoCAD) and a proven ability to support both detailed engineering and field execution.",
+        "I’m just a person trying to make sense of the world through engineering: by building, experimenting, and occasionally breaking things (on purpose or by accident, depends on the day). This site is a mix of experiments, projects, and thoughts. I enjoy taking on projects, solving problems, and picking up new things all while i figure out how stuff works. I the kind of guy who would figure things out as we go about the project. ",
 
-        "Worked on AWS service platforms for machine learning and data engineering for process optimization and computing. I have experience deploying AI solutions, in cooperating machine learning technologies to meet engineering problem statements like predictive maintenance and anomaly detection.",
+        "I believe engineering is a means to an end. My goal is the end. So I guess, whatever it takes to get the job done. I also believe in hard work and always caughing up with technology with ever-ongoing learning to get there.",
 
-        "Let us connect if you are looking for a go-getter engineer passionate about delivering technically reliable, cost-effective solutions for the oil and gas industry.",
+        "I love tackling challenges, whether it’s with code, machines, or with duct tape. I mess around with ideas, trying to turn them into reality. Chasing projects that start with “what if…” that somehow turn into projects that keep me up at 2am. I built this site to document my journey, share things about me, and maybe connect with folks who get excited about the same stuff.",
 
         "Also Did I mention I'm an Aggie Engineer!"
     ],
@@ -40,7 +39,7 @@ const siteData = {
         linkedin: "https://www.linkedin.com/in/fernandes-isaiah/",
         email: "mailto:isaiahfernandes0777@gmail.com"
     }
-};``
+};
 
 // Experience data
 const experiences = [
@@ -77,10 +76,7 @@ const experiences = [
         tags: [
             "✨ AWS",
             "✨ Python",
-            "✨ Boto3",
-            "Terraform",
-            "Packer",
-            "Pytest"
+            "✨ Boto3"
         ]
     },
     {
@@ -128,217 +124,153 @@ const experiences = [
             "✨ MS - Projects",
         ]
     },
-//    {
-//        type: "experience",
-//        link: "https://www.vsp.com/",
-//        period: "Aug 2017 - May 2019",
-//        title: "Teaching Assistant at Sac State",
-//        description:
-//            "Helped, guided, and graded students' assignments and answered " +
-//            "questions during online discussions.",
-//        tags: [
-//            "✨ Advanced Computer 3D Graphics",
-//        ]
-//    }
 ];
-
-
-
 
 // Project data
 const projects = [
     {
         type: "project",
         link:
-            "https://github.com/ajinkyamalhotra/Steam-Game-Recommendation/",
-        period: "Mar 2023 - May 2023",
-        title: "Steam Game Recommendation",
+            "",
+        period: "July 2025 - Sept 2025",
+        title: "Financial Data Reporting Automation",
         description:
-            "Developed a video game recommendation engine utilizing " +
-            "Content-Based Filtering and VBPR algorithms. Successfully " +
-            "deployed the recommendation system which enhanced user " +
-            "engagement and satisfaction by streamlining the discovery " +
-            "process for games tailored to individual tastes.",
+            "Developed AI agents to generate, verify, compile data, increase accuracy, eliminating review, and automated fiscal quarters data workflows.",
         tags: [
-            "AWS Lambda",
-            "Heroku",
-            "Python",
-            "HTML/CSS",
-            "Bootstrap",
-            "Git"
+            "Machine Learning",
+            "Agentic AI",
+            "Project leading",
+            "✨ Python",
+            "✨ AWS",
         ]
     },
     {
         type: "project",
-        link: "https://github.com/ajinkyamalhotra/Chess-Master",
-        period: "Feb 2023 - Apr 2023",
-        title: "Chess Master",
+        link:
+            "",
+        period: "June 2025 - August 2025",
+        title: "Financial Executive Chatbot",
         description:
-            "Designed a human vs CPU chess-like game and created a " +
-            "computer player, using Minimax algorithm. CPU player is " +
-            "optimized through Alpha-Beta pruning, Depth Limited Search, " +
-            "and Iterative Deepening Search.",
-        tags: ["✨ Java", "Minimax", "DLS", "IDS"]
+            "Built an AI-driven chatbot enabling executives to query financial insights across multiple structured categories across their entire database.",
+        tags: [
+            "Machine Learning",
+            "Agentic AI",
+            "✨ Python",
+            "✨ AWS",
+        ]
+    },
+    {
+        type: "project",
+        link: "",
+        period: "Aug 2023 - Dec 2023",
+        title: "Petroleum Project Evaluation",
+        description:
+            "Determining reserves, forecasted production, and modeled cash flows to evaluate project profitability and guide investment decisions effectively",
+        tags: ["Reserves Evaluation", "Production Forecasting", "Cash Flow Modeling" , "Well Economic", "Economic Indicators","✨ Excel"]
     },
     {
         type: "project",
         link:
-            "https://github.com/ajinkyamalhotra/Network-Optimization-using-" +
-            "Shortest-Path",
-        period: "Nov 2022",
-        title: "Network Optimization",
+            "",
+        period: "Jan 2023 - May 2023",
+        title: "Design of a Pipeline",
         description:
-            "Designed a sparse (30000 total edges) and dense (5000000 total " +
-            "edges) random graph generator. Implemented Dijkstras without " +
-            "heap, with heap and Kruskals algorithm for performance " +
-            "comparison.",
-        tags: ["✨ Java", "Dijkstras", "Heap", "Kruskals"]
+            "Design of a offshore pipeline mechanically as per API 1111 ",
+        tags: ["Wall Thickness Design", "Stress & Buckling Analysis" ,"Thermal & Expansion Design" ,"Material Selection", "✨ Orca Flex"]
     },
     {
         type: "project",
         link:
-            "https://github.com/ajinkyamalhotra/Firearm-Classification",
-        period: "Oct 2019 - Dec 2019",
-        title: "Firearm Classification",
+            "",
+        period: "Aug 2022 - Dec 2022",
+        title: "Kyle offshore field development",
         description:
-            "Designed a Neural Network (NN) using TFLearn to classify " +
-            "different types of firearms. Achieved 85% accurate results for " +
-            "1000 epochs using the Alex NET architecture.",
-        tags: ["✨ Python", "CNN", "TensorFlow", "TFLearn"]
+            "Prefeed Feed project on a potential Oil and Gas Endeavor.",
+        tags: ["Pre-FEED","","✨ Excel"]
     },
     {
         type: "project",
         link:
-            "https://github.com/ajinkyamalhotra/GpuProgrammingCuda/blob/" +
-            "master/TiledMatrixMultiplicationKernel.cu",
-        period: "May 2018",
-        title: "2D Tiled Image Convolution",
+            "",
+        period: "Jan 2023 - May 2023",
+        title: "Flow Assurance",
         description:
-            "Implemented a tiled image convolution CUDA kernel which " +
-            "utilizes shared memory and constant memory. Input data is " +
-            "loaded/titled from global memory to shared memory, to reduce " +
-            "the number of reads. Constant memory is the fastest memory in a " +
-            "GPU grid and is used to store the constant mask data.",
-        tags: ["✨ CUDA", "C", "Parallel GPU Programming"]
+            "Design of a pipeline to combat hydrates and assphaltenes during shut ins and blow downs.",
+        tags: [""]
     },
     {
         type: "project",
         link:
-            "https://github.com/ajinkyamalhotra/GpuProgrammingCuda/blob/" +
-            "master/HistogramKernel.cu",
-        period: "Apr 2018",
-        title: "Histogram",
+            "",
+        period: "Aug 2020 - May 2021",
+        title: "Mechanical Robo",
         description:
-            "Implemented an efficient Histogram algorithm using " +
-            "privatization technique for an input array of Integers. " +
-            "4096 Histogram bins use unsigned 32-bit counters that are " +
-            "saturated at 127.",
-        tags: ["✨ CUDA", "C", "Parallel GPU Programming"]
+            "Design and Fabrication of a robot to assist with solar panels peak functionality.",
+        tags: [""]
     },
     {
         type: "project",
         link:
-            "https://github.com/ajinkyamalhotra",
-        period: "Mar 2017 - Jul 2017 ",
-        title: "Attendance Tracker",
+            "",
+        period: "Jan 2019 - May 2019 ",
+        title: "Design of a Visco Meter",
         description:
-            "Created an application by using Google Sheets API to record " +
-            "attendance on Google docs spreadsheet. Developed a portal for " +
-            "students to register their attendance and for professors to " +
-            "keep track of attendance.",
-        tags: ["✨ Java", "Google Sheets API", "HTML/CSS", "JavaScript"]
+            "Design and Fabrication of a viscometer to measure the viscous index of various liquids.",
+        tags: [""]
     }
 ];
 
 const skills = [
     {
-        name: "AWS",
+        name: "Mechanical Design",
         level: 8.5,
         subSkills: [
-            { name: "Lambda", level: 9 },
-            { name: "API Gateway", level: 9 },
-            { name: "SNS", level: 9 },
-            { name: "SQS", level: 9 },
-            { name: "IAM", level: 9 },
-            { name: "EC2", level: 9 },
-            { name: "S3", level: 9 },
-            { name: "EBS", level: 8 },
-            { name: "EFS", level: 8 },
-            { name: "Spot Fleets", level: 8 },
-            { name: "ASG", level: 8 },
-            { name: "CloudFormation", level: 8 },
-            { name: "DynamoDB", level: 8 },
-            { name: "Terraform", level: 7 },
-            { name: "Packer", level: 7 },
-            { name: "RDS", level: 7 },
-            { name: "Connect", level: 6 },
-            { name: "Pinpoint", level: 6 },
+            { name: "Fusion 360", level: 9 },
+            { name: "DS-Solidworks 2019", level: 7.5 },
+            { name: "DS-Solidworks 2020", level: 7 }
         ]
     },
     {
-        name: "Software Engineering",
+        name: "Simulation Engineering",
+        level: 7.5,
+        subSkills: [
+            { name: "ANSYS Mechanical", level: 7.5 },
+            { name: "ANSYS Fluent/CFX", level: 6.5 }
+        ]
+    },
+    {
+        name: "Project Engineering",
         level: 8,
         subSkills: [
-            { name: "Algorithms & Data Structures", level: 8 },
-            { name: "Object Oriented Programming", level: 8 },
-            { name: "APIs", level: 8 },
-            { name: "Scrum", level: 8 },
-            { name: "CI/CD", level: 8 },
-            { name: "Test Automation", level: 7 },
-            { name: "Security", level: 7 },
-            { name: "Software Development Life Cycle (SDLC)", level: 7 }
+            { name: "MS-Projects", level: 8 }
         ]
     },
     {
-        name: "Operating Systems",
-        level: 8,
-        subSkills: [
-            { name: "Windows", level: 8 },
-            { name: "Linux/Unix", level: 9 },
-            { name: "MacOS", level: 8 }
-        ]
-    },
-    {
-        name: "IDEs",
+        name: "Oil & Gas",
         level: 7.5,
         subSkills: [
-            { name: "Eclipse", level: 7.5 },
-            { name: "IntelliJ", level: 8.5 },
-            { name: "Visual Studio", level: 9 },
-            { name: "MySQL Workbench", level: 7 }
+            { name: "OLGA", level: 7.5 },
+            { name: "Orca Flex", level: 8 }
         ]
     },
     {
-        name: "Coding Languages",
+        name: "AWS",
         level: 7.5,
         subSkills: [
-            { name: "Python", level: 8 },
-            { name: "Java", level: 9 },
-            { name: "JavaScript", level: 8 },
-            { name: "Groovy", level: 9 },
-            { name: "Bash", level: 8 },
-            { name: "SQL", level: 8 },
-            { name: "C/C++", level: 4.5 },
+            { name: "Lambda", level: 6.5 },
+            { name: "Bedrock", level: 7 },
+            { name: "S3", level: 7 },
+            { name: "EC2", level: 6 },
+            { name: "RDS", level: 6.5 }
         ]
     },
     {
-        name: "Tools",
+        name: "Languages",
         level: 7.5,
         subSkills: [
-            { name: "Git", level: 8 },
-            { name: "Jira", level: 8 },
-            { name: "Confluence", level: 8 },
-            { name: "Jenkins", level: 8 },
-            { name: "Splunk", level: 7 },
-        ]
-    },
-    {
-        name: "Machine Learning",
-        level: 5,
-        subSkills: [
-            { name: "TensorFlow", level: 5 },
-            { name: "TFLearn", level: 5 },
-            { name: "CNN (Convolutional Neural Networks)", level: 5 }
+            { name: "Python", level: 7 },
+            { name: "C/C++", level: 6.5 },
+            { name: "Java", level: 5 }
         ]
     },
 ];
@@ -346,45 +278,45 @@ const skills = [
 // New Timeline Data (for Timeline feature)
 const timelineData = [
     {
-        title: "Bachelor's of Science in Computer Science",
-        period: "Jan 2015 - May 2019",
-        info: "Studied Computer Science at California State University.",
-        year: "2015"
+        title: "BEng",
+        period: "Aug 2017 - May 2021",
+        info: "Studied Mechanical Engineering at Mumbai University.",
+        year: "2017"
     },
     {
-        title: "1st SDE Internship",
-        period: "June 2018 - May 2019",
-        info: "Got my first internship opporunity at VSP Vision Care.",
-        year: "2018"
-    },
-    {
-        title: "1st SDE Job",
+        title: "1st project leading experience",
         period: "August 2019 - May 2020",
-        info: "Started my professional journey as a SDE at Esurance.",
+        info: "Got my first project leading exposure as a junior level project manager.",
         year: "2019"
     },
     {
-        title: "Cloud Support Engineer at AWS",
-        period: "May 2020 - June 2022",
-        info: "Transitioned to AWS, where I took on advanced cloud/engineering challenges.",
+        title: "1st mechanical Job",
+        period: "August 2020 - May 2021",
+        info: "Worked on building a car as well as figuring out ways to sponsor it.",
         year: "2020"
     },
     {
-        title: "Master's Degree in Computer Science",
-        period: "August 2022 - December 2023",
-        info: "Went back to school to gain my Master's in Computer Science at Texas A&M University.",
+        title: "MEng",
+        period: "August 2022 - May 2024",
+        info: "Went to school to gain my Master's at Texas A&M University.",
         year: "2022"
     },
     {
-        title: "SDE Internship @ ARM",
-        period: "May 2023 - August 2023",
-        info: "While pursuing my masters degree, I interned at ARM as a SDE.",
-        year: "2023"
+        title: "Analytics Engineering",
+        period: "Nov 2024 - May 2025",
+        info: "Research and Development into optimizing oil and gas processes.",
+        year: "2024"
     },
     {
-        title: "SDE @ ARM",
-        period: "Jan 2024 - Present",
-        info: "Working as a Software Engineer at ARM, focusing on high-impact projects.",
+        title: "Data Engineering",
+        period: "June 2024 - June 2024",
+        info: "Focusing on making small buisnesses more profitable.",
+        year: "2024"
+    },
+    {
+        title: "ML Engineering",
+        period: "June 2024 - June 2024",
+        info: "Optimizing engineering processes by in-cooperating AI.",
         year: "2024"
     }
 ];
